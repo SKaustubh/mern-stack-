5ffb3e3c9aec0b6910541bfe332031f065efbad3
Kaustubh <kaustubhsingh37@gmail.com>
2023-07-26 20:02:10 +0530

    end commit

--- section 1/arrayTraverse.js
+++ section 1/arrayTraverse.js
@@ -1,12 +1,16 @@
-const nums=[3,5,7,3,9,10];
+const nums = [3, 5, 7, 3, 9, 10];
 
-for(let i=0;i<nums.length;i++){
-    console.log(nums[i] ** 2);
+for (let i = 0; i < nums.length; i++) {
+  console.log(nums[i] ** 2);
 }
 
 console.log("===================*************========================");
 // for of loop
 
-for(let num of nums){
-    console.log(num ** 2);
+for (let num of nums) {
+  console.log(num ** 2);
+}
+console.log("===================*************========================");
+for (let i of nums.slice(0, 3)) {
+  console.log(i);
 }
