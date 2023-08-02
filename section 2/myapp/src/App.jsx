4d95178aee2dce4560995dbe9c4745c8cec14bc9
Kaustubh <kaustubhsingh37@gmail.com>
2023-08-02 20:00:07 +0530

    react

---
+++ section 2/myapp/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <div>
+      <h1>kya hain sachin main!?</h1>
+      <Home />
+      <Login />
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
---
+++ section 2/myapp/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>lappu sa sachin hain</h1>
+      <h2>bolna atta nhin</h2>
+      <h3>deth footiya hain</h3>
+    </div>
+  );
+};
+
+export default Home;
